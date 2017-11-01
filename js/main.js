578f72f510f5332efe98cf84b351e6aacbc00542
Dharma Sanjyal <rajsan44@gmail.com>
2017-11-01 12:24:59 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//when the page is ready. do this
+$(document).ready(function() {
+	//when the div element is clicked, do this
+	$().click(function() {
+		//toggle the class of fonts to all li
+		$().hide()
+		});
+	})
